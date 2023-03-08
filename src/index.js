89ea1f4951000bc59c292f2a77189cce0efddc09
Daniel Bevan <daniel.j.bevan96@gmail.com>
2023-03-08 11:29:07 +0100

    Added react-toastify library so we can use toast notifications anywhere in the applciation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import App from './App';
 import { ContextProvider } from './context/ContextProvider';
@@ -10,6 +12,7 @@ root.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
+      <ToastContainer />
     </ContextProvider>
   </React.StrictMode>
 );
